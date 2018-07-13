3f0dafe55f65331a9b6a29a222656fde633b09d7
SunnySunhwa <via1229@gmail.com>
2018-07-13 18:31:29 +1200

    changed 'Smart' componenet into 'Dumb' componenet

--- src/App.js
+++ src/App.js
@@ -4,33 +4,52 @@ import './App.css';
 import Movie from './Movie';
 
 
-const movies = [
-  {
-    title: "Avengers",
-    poster: "https://images-na.ssl-images-amazon.com/images/I/719S7I-%2ByBL._SL1023_.jpg"
-  },
-  {
-    title: "The Ant man",
-    poster: "http://www.joblo.com/posters/images/full/ant-man-international-2.jpg"
-  },
-  {
-    title: "Rampage",
-    poster: "https://ae01.alicdn.com/kf/HTB1RF0gfKySBuNjy1zdq6xPxFXay/Custom-Canvas-Wall-Decals-Monster-Movie-King-Kong-Rampage-Poster-Rampage-Stickers-Dwayne-Johnson-Wallpaper-Office.jpg"
-  },
-  {
-    title: "Get out",
-    poster: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT8F5h_cwya5bQrqunsTix3zPkEUyEGszUk3kX-NjOrlzpMuPnU"
-  },
-];
-
-
 class App extends Component {
+
+  state = {
+  }
+
+  componentDidMount(){
+    setTimeout(() => {
+      this.setState({
+       movies: [{
+           title: "Avengers",
+           poster: "https://images-na.ssl-images-amazon.com/images/I/719S7I-%2ByBL._SL1023_.jpg"
+         },
+         {
+           title: "The Ant man",
+           poster: "http://www.joblo.com/posters/images/full/ant-man-international-2.jpg"
+         },
+         {
+           title: "Rampage",
+           poster: "https://ae01.alicdn.com/kf/HTB1RF0gfKySBuNjy1zdq6xPxFXay/Custom-Canvas-Wall-Decals-Monster-Movie-King-Kong-Rampage-Poster-Rampage-Stickers-Dwayne-Johnson-Wallpaper-Office.jpg"
+         },
+         {
+           title: "Get out",
+           poster: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT8F5h_cwya5bQrqunsTix3zPkEUyEGszUk3kX-NjOrlzpMuPnU"
+         },
+         {
+            title: "incredibles2",
+            poster: "https://images.penguinrandomhouse.com/cover/9780736438506"
+          }
+        ]
+      });
+    }, 2000)
+  }
+
+  _renderMovies = () => {
+     const movies = this.state.movies.map((movie, idx) => {
+         return <Movie title = { movie.title }
+         poster = { movie.poster } key = { idx} />
+       })
+    return movies;
+  }
+
+
   render() {
     return (
       <div className="App">
-      {movies.map((movie, idx) => {
-        return <Movie title={movie.title} poster={movie.poster} key={idx}/>
-      })}
+        {this.state.movies ? this._renderMovies() : "loading!"}
       </div>
     );
   }
--- src/Movie.js
+++ src/Movie.js
@@ -1,17 +1,51 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import './Movie.css';
 
+
+function Movie({title, poster}){
+  return(
+    <div>
+      <MoviePoster poster={poster}/>
+      <h1> {title}</h1>
+    </div>
+  );
+} 
+
+function MoviePoster({poster}){
+  return(
+    <img src={poster} alt="Movie Poster" />
+  )
+}
+
+
+Movie.propTypes = {
+  title: PropTypes.string.isRequired,
+  poster: PropTypes.string.isRequired
+}
+
+
+MoviePoster.propTypes = {
+  poster: PropTypes.string.isRequired
+}
+
+export default Movie;
+
+
+/* in case of "Smart" componenet
+
 class Movie extends Component{
 
+  // for data type checking 
   static propTypes = {
+    //isRequired = 'Movie' has to have a title(string) as a prop 
     title: PropTypes.string.isRequired,
     poster: PropTypes.string
   }
 
 
   render(){
-    console.log(this.props)
+  //  console.log(this.props) // {title, poster}
     return(
       <div>
         <MoviePoster poster={this.props.poster}/>
@@ -23,13 +57,10 @@ class Movie extends Component{
 
 class MoviePoster extends Component{
   render(){
-    console.log(this.props)
+  //  console.log(this.props) // {poster}
     return(
-      <img src={this.props.poster}/>
+      <img src={this.props.poster} alt="Movie Poster" />
     );
   }
 }
-
-
-
-export default Movie;
+*/
